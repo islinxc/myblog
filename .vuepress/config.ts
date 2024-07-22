efef0932585f02b0293ae19ed0e7968c5daeab69
会飞的猫 <islinxc@163.com>
2024-07-22 15:36:37 +0800

    文章：python函数进阶笔记

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -56,7 +56,7 @@ export default defineUserConfig({
       "/docs/Python/": [
         {
           text: "Python语言",
-          children: ["1", "2", "3", "4", "5"],
+          children: ["1", "2", "3", "4", "5", "12"],
         },
       ],
     },
